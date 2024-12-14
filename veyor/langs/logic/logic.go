b9db1a6bbce06e1b2b29016c527dd5268bfe8b24
Stephen Malone <mail@wirehaiku.org>
2024-12-14 12:53:15 +1000

    Added logic.Evaluate* functions.

--- veyor/langs/logic/logic.go
+++ veyor/langs/logic/logic.go
@@ -2,10 +2,52 @@
 package logic
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/stvmln86/veyor/veyor/atoms/atom"
+	"github.com/stvmln86/veyor/veyor/atoms/cell"
+	"github.com/stvmln86/veyor/veyor/atoms/word"
+	"github.com/stvmln86/veyor/veyor/langs/queue"
+	"github.com/stvmln86/veyor/veyor/langs/stack"
+	"github.com/stvmln86/veyor/veyor/opers"
 )
+
+// Evaluate evaluates an Atom against a Queue and Stack.
+func Evaluate(a atom.Atom, q *queue.Queue, s *stack.Stack) error {
+	switch a := a.(type) {
+	case cell.Cell:
+		s.Push(a)
+		return nil
+
+	case word.Word:
+		f, ok := opers.Opers[a]
+		if !ok {
+			return fmt.Errorf("invalid operator %q", a)
+		}
+
+		return f(q, s)
+
+	default:
+		return fmt.Errorf("invalid Atom type %T", a)
+	}
+}
+
+// EvaluateQueue evaluates a Queue against a Stack.
+func EvaluateQueue(q *queue.Queue, s *stack.Stack) error {
+	for !q.Empty() {
+		a, err := q.Dequeue()
+		if err != nil {
+			return err
+		}
+
+		if err := Evaluate(a, q, s); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
 
 // Parse returns an Atom slice from a string.
 func Parse(s string) ([]atom.Atom, error) {
--- veyor/langs/logic/logic_test.go
+++ veyor/langs/logic/logic_test.go
@@ -7,7 +7,39 @@ import (
 	"github.com/stvmln86/veyor/veyor/atoms/atom"
 	"github.com/stvmln86/veyor/veyor/atoms/cell"
 	"github.com/stvmln86/veyor/veyor/atoms/word"
+	"github.com/stvmln86/veyor/veyor/langs/queue"
+	"github.com/stvmln86/veyor/veyor/langs/stack"
 )
+
+func TestEvaluate(t *testing.T) {
+	// setup
+	s := stack.New(1)
+
+	// success - Cell
+	err := Evaluate(cell.Cell(2), nil, s)
+	assert.Equal(t, []cell.Cell{1, 2}, s.Cells)
+	assert.NoError(t, err)
+
+	// success - Word
+	err = Evaluate(word.Word("+"), nil, s)
+	assert.Equal(t, []cell.Cell{3}, s.Cells)
+	assert.NoError(t, err)
+
+	// error - invalid Atom type
+	err = Evaluate(nil, nil, nil)
+	assert.EqualError(t, err, `invalid Atom type <nil>`)
+}
+
+func TestEvaluateQueue(t *testing.T) {
+	// setup
+	q := queue.New(cell.Cell(2), word.Word("+"))
+	s := stack.New(1)
+
+	// success
+	err := EvaluateQueue(q, s)
+	assert.Equal(t, []cell.Cell{3}, s.Cells)
+	assert.NoError(t, err)
+}
 
 func TestParse(t *testing.T) {
 	// success
